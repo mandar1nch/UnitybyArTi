b460692e76e021be83bf908441f1a1610daeea4d
mandar1nch <99121870+mandar1nch@users.noreply.github.com>
2022-07-25 15:48:19 +0200

    Add files via upload

---
+++ Assets/playerresource2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+
+
+public class playerresource2 : MonoBehaviour
+{
+    public static playerresource2 Find(string name)
+    {
+        foreach (var resource in FindObjectsOfType<playerresource2>())
+        {
+            if (resource.resourceName == name)
+            {
+                return resource;
+            }
+        }
+        return null;
+    }
+
+    [SerializeField] private float value = 0;
+    [SerializeField] private float maxValue = 9999; // ДОБАВЛЕНО
+    public float GetValue() => value;
+    public float GetMaxValue() => maxValue; // ДОБАВЛЕНО
+
+    public string resourceName = "money";
+
+    public void ChangeValue(float change)
+    {
+        value = Mathf.Clamp(value + change, 0, maxValue); // ИЗМЕНЕНО
+        if (ValueChangeEvent != null)
+        {
+            ValueChangeEvent(value);
+        }
+    }
+
+    public event Action<float> ValueChangeEvent;
+}
---
+++ Assets/resourcessource.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+public class resourcessource : MonoBehaviour
+{
+    public string resourceName;
+    public float value;
+
+    public void CollectResource(bool destroyObject)
+    {
+        playerresource2.Find(resourceName).ChangeValue(value);
+        if (destroyObject)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
